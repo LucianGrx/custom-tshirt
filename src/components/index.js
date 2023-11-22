3081e7443e990f9f393256edcc2b76bb35965019
luciangrx <luciangrr@gmail.com>
2023-11-22 15:29:47 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import CustomButton from "./CustomButton";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+
+export { CustomButton, Tab, FilePicker, ColorPicker};
